8b720e3a0b0fc6303e2f3c43877427d1752cd5b3
Felipe <felipecoimbra97@gmail.com>
2019-04-04 20:39:26 -0300

    Add BSD Packet Filter and receive parameters from cmd line

--- src/main_sniffing.c
+++ src/main_sniffing.c
@@ -1,43 +1,70 @@
 #include "sniffing.h"
 
 #include <stdlib.h>
+#include <stdarg.h>
 
-void print_usage() {
-    printf("Usage: sniffing <NETWORK INTERFACE>\n");
+void exit_error(int code, ...) {
+    va_list args;
+    switch (code)
+    {
+        case 1:
+            printf("Usage: sniffing <NETWORK INTERFACE> <FILTER SYNTAX>[optional]\n");
+            break;
+        
+        case 2:
+            printf("Error: Could not find device %s\n", args[1]);
+
+        default:
+            printf("Unknown error code: %d", code);
+    }
+    exit(code);
 }
 
 /*
     Args:
         1 - Network Interface identifier
+        2 - Filter syntax
 */
 int main(int argc, char* argv[]) {
 
     if (argc < 2) {
-        print_usage();
-        exit(1);
+        exit_error(1);
     }
 
-    pcap_t *handle;
-    char errbuf[PCAP_ERRBUF_SIZE];
-    struct bpf_program fp;
-    
     //
     //  Network interface identification
     //
-    const char* NETWORK_INTERFACE = argv[1];
+    char* net_interface = argv[1];
+    
+    //
+    // BPF pcap syntax
+    //
+    char* filter_exp = NULL;
+
+    if (argc > 2) {
+        printf("oi");
+        filter_exp = argv[2];
+    }
 
     //
-    //  BPF Filter Setup
+    // Pcap Session Setup
     //
-    char filter_exp[] = "ip proto icmp";
+    pcap_t *handle;
+    char errbuf[PCAP_ERRBUF_SIZE];
+
+    // Step 1: Open live pcap session on NIC with name net_interface
+    handle = pcap_open_live(net_interface, BUFSIZ, 1, 1000, errbuf);
+
+    //
+    //  BPF Setup
+    //
+    const char default_filter_exp[] = "ip";//"ip proto icmp";
+    struct bpf_program fp;
     bpf_u_int32 net;
 
-    // Step 1: Open live pcap session on NIC with name NETWORK_INTERFACE
-    handle = pcap_open_live("wlp3s0", BUFSIZ, 1, 1000, errbuf);
-
     // Step 2: Compile filter_exp into BPF psuedo-code
-    // pcap_compile(handle, &fp, filter_exp, 0, net);
-    // pcap_setfilter(handle, &fp);
+    pcap_compile(handle, &fp, filter_exp != NULL ? filter_exp : default_filter_exp, 0, net);
+    pcap_setfilter(handle, &fp);
     
     // Step 3: Capture packets
     pcap_loop(handle, -1, simple_callback, NULL);
@@ -47,5 +74,3 @@ int main(int argc, char* argv[]) {
     
     return 0;
 }
-    // Note: don’t forget to add "-lpcap" to the compilation command.
-    // For example: gcc -o sniff sniff.c -lpcap
